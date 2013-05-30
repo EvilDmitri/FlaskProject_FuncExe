79ef0946111d7ed97c13b15ce7187ea2e34d12f9
EvilDmitri <dmitribrach@gmail.com>
2013-05-30 23:43:34 +0300

    some job about parser

--- FlaskApp/modules/parser.py
+++ FlaskApp/modules/parser.py
@@ -1,52 +1,110 @@
 from patterns import patterns
 import re
 
+class Value:
+    def __init__(self):
+        self.n_stroke = ''  # number of stroke in content
+        self.id = ''        # name of element
+        self.val = ''     # value from form
 
-values = {}
-html = []
+
+class Parser:
+
+    def __init__(self):
+        self.values = []    # List of all macro elements
+        self.html = []      # HTML-tags from macros
+        self.content = []   # List of lines in file
+        self.results = []   # List of results received from form 'post'
+        self.output = ''    # Type of output
 
 
-def parse(filename):
-    content = []
-    with open(filename) as f:
-        for line in f:
-            if '\n' == line:
-                pass
-            else:
-                content.append(line)
 
-    with open(filename+'tmp', 'w') as fw:
-        for line in content:
+    def load(self, filename):
+        """Load all file content into list
+        :param filename: name of file to parse
+        """
+        with open(filename) as f:
+            for line in f:
+                if '\n' == line:
+                    pass
+                else:
+                    self.content.append(line)
+
+    def get_macros(self):
+        """
+            Retrieve macros from file content
+        """
+        for number, line in enumerate(self.content):
+
+            # for pattern in patterns.iterkeys():
             if 'INPUTFIELD_TXT' in line:
-                match = re.search(r'INPUTFIELD_TXT\((.+?)\)', content)
-                text = match.grop(1).split(', ')
-                values[name] = ''
+
+                # New value
+                value = Value()
+
+                # Find the content of macro
+                match = re.search(r'INPUTFIELD_TXT\((.+?)\)', self.content)
+
+                # Get array of user defined fields
+                text = match.group(1).split(', ')
+
+                # The name of user defined element (Maybe it should not be defined by user?)
+                value.id = text.group(1)
+
+                value.n_stroke = number
+
+                # Add new macro into final list
+                self.values.append(value)
+
+                # Paste fields into html-tag
                 tag = '{desc} <input type="text" name="{name}">'.format(desc=text[1], name=text[0])
-                html.append(tag)
 
-                line = line.replace(a.group(0), a.group(1))
+                # Add tag to html list
+                self.html.append(tag)
 
-            fw.write(line)
+                # Paste name of macro into line and line into the content
+                self.content[number] = line.replace(match.group(0), value.id)
 
+    def get_html(self, filename):
+        self.load(filename)
+        self.get_macros()
 
-def insert_values(filename):
-
-    with open(filename) as fr:
-        lines = fr.readlines()
-
-    with open(filename, 'w') as fw:
-        for line in lines:
-            for key in values.iterkeys():
-                if key in line:
-                    line = line.replace(key, values[key])
-
-            fw.write(line)
+        return self.html
 
 
+    def insert_values(self):
+        for value in self.values:
+            line = self.content[value.n_stroke]
+            self.content[value.n_stroke] = line.replace(value.id, value.val)
 
-def show_html(filename):
-    html = '''<form action="result">
-                <br>
-                <br>
-<input type="submit" value="Submit">
-</form>'''
+
+    def put_results(self, results):
+        """ Put data in values
+        :param results: dictionary with results from form response
+        """
+        for result in results.iterkey():
+
+
+
+    def run(self):
+        for value in self.values:
+            pass
+
+
+
+
+
+
+    def get_output(self):
+        output = self.content[-1]
+
+
+
+
+
+    def show_html(self, filename):
+        html = '''<form action="result">
+                    <br>
+                    <br>
+    <input type="submit" value="Submit">
+    </form>'''
